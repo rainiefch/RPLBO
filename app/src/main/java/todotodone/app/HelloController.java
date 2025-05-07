075d82f929734a3c3276e7453b0e961c7e64ab4a
rainiefch <143920728+rainiefch@users.noreply.github.com>
2025-05-07 08:16:53 +0700

    yes

---
+++ app/src/main/java/todotodone/app/HelloController.java
@@ -0,0 +1,14 @@
+package todotodone.app;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
