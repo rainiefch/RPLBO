d771da2a64d49360955c017c32f19ef6a62b2c7b
mariajacynthia <143603331+mariajacynthia@users.noreply.github.com>
2025-05-07 10:51:56 +0700

    loginregis

--- demo/src/main/java/todotodone/app/demo/ToDoApplication.java
+++ demo/src/main/java/todotodone/app/demo/ToDoApplication.java
@@ -11,7 +11,7 @@ public class ToDoApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(ToDoApplication.class.getResource("login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("To-Do To Done");
         stage.setScene(scene);
         stage.show();
