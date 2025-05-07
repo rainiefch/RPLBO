c60f2278dd6c4fa1ba817645d02ada2b17cc9468
rainiefch <143920728+rainiefch@users.noreply.github.com>
2025-05-07 08:37:47 +0700

    asda

--- app/src/main/java/todotodone/app/ToDoToDoneApp.java
+++ app/src/main/java/todotodone/app/ToDoToDoneApp.java
@@ -1,16 +1,23 @@
 package todotodone.app;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class ToDoToDoneApp extends Application {
+import java.io.IOException;
 
-    public static void main(String[] args) {
-        launch(args);
+public class ToDoToDoneApp extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("To-Do to Done");
+        stage.setScene(scene);
+        stage.show();
     }
 
-    @Override
-    public void start(Stage primaryStage) {
-
+    public static void main(String[] args) {
+        launch();
     }
 }
