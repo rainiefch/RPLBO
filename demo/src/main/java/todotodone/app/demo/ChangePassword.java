c05614e8911fdb0ed923533093e5ec07c90ff5a7
memel25 <145663112+memel25@users.noreply.github.com>
2025-05-08 15:43:32 +0700

    push

--- demo/src/main/java/todotodone/app/demo/ChangePassword.java
+++ demo/src/main/java/todotodone/app/demo/ChangePassword.java
@@ -1,4 +1,36 @@
 package todotodone.app.demo;
 
-public class ChangePassword {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class ChangePassword{
+
+    @FXML
+    private Button btnCancel;
+
+    @FXML
+    private Button btnChangePass;
+
+    @FXML
+    private PasswordField txtConfPass;
+
+    @FXML
+    private PasswordField txtNewPass;
+
+    @FXML
+    private TextField txtUsername;
+
+    @FXML
+    void onBtnCancelClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onBtnChangePassClick(ActionEvent event) {
+
+    }
+
 }
--- demo/src/main/java/todotodone/app/demo/Login.java
+++ demo/src/main/java/todotodone/app/demo/Login.java
@@ -1,4 +1,59 @@
 package todotodone.app.demo;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
 public class Login {
+
+    @FXML
+    private Button btnForgotPass;
+
+    @FXML
+    private Button btnSignIn;
+
+    @FXML
+    private Label lblPassword;
+
+    @FXML
+    private Label lblRegister;
+
+    @FXML
+    private Label lblRplbo;
+
+    @FXML
+    private Label lblToDoToDone;
+
+    @FXML
+    private Label lblUsername;
+
+    @FXML
+    private PasswordField pfPassword;
+
+    @FXML
+    private ImageView picture;
+
+    @FXML
+    private TextField tfUsername;
+
+    @FXML
+    void onBtnForgotPassClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onBtnSignInClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onLblRegisterClick(MouseEvent event) {
+
+    }
+
 }
